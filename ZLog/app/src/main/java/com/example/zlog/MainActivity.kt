101d2f3880fb5da8fd14fce0f068afd76631c8c8
jason_zht <zht505658779@gmail.com>
2022-10-26 08:21:06 +0800

    第三次测试

--- ZLog/app/src/main/java/com/example/zlog/MainActivity.kt
+++ ZLog/app/src/main/java/com/example/zlog/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.zlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jasonzhou.zlog.handler.CrashHandler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CrashHandler.INSTANCE.init(this)
     }
 }
